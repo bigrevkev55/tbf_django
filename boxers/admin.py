a4c4981b89f1fb8a3c4aedcbb1d91ef12516b064
bigrevkev55 <bigrevkev55@yahoo.com>
2021-05-02 19:03:24 -0500

    Added fights table, updated requirements file

--- boxers/admin.py
+++ boxers/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Genre, Boxer
+from .models import Genre, Boxer, Fights
 
 
 class GenreAdmin(admin.ModelAdmin):
@@ -9,7 +9,11 @@ class GenreAdmin(admin.ModelAdmin):
 class BoxerAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'nick_name', 'genre')
 
+class FightsAdmin(admin.ModelAdmin):
+    list_display = ('red_boxer','blue_boxer','scheduled_rounds','result_round','fight_result','ight_winner')
+
 
 # Register your models here.
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Boxer, BoxerAdmin)
+admin.site.register(Fights, FightsAdmin)
